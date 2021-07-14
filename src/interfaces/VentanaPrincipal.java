119d1e1a17cf2952d70eaa6ba8d8d420e813ace1
juan312 <juanalbani48@gmail.com>
2021-07-14 12:21:21 -0300

    Interfaces 0.1

---
+++ src/interfaces/VentanaPrincipal.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+import javax.swing.*;
+
+public class VentanaPrincipal extends JFrame {
+
+	public VentanaPrincipal() {
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setSize(400,200);
+		this.pack();
+		this.setVisible(true);
+	}
+	
+}
